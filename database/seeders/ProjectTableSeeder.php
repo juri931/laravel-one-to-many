7205ed243dc4240af0cd681a0a7d241b0361455c
juri931 <impa.mat@hotmail.it>
2024-05-28 11:37:51 +0200

    "Moved ProjectTableSeeder call in DatabaseSeeder, added Type model and type_id assignment in ProjectTableSeeder"

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Models\Project;
 use App\Functions\Helper;
+use App\Models\Type;
 
 class ProjectTableSeeder extends Seeder
 {
@@ -17,6 +18,7 @@ class ProjectTableSeeder extends Seeder
     {
         for($i = 0; $i < 20; $i++){
             $new_project = new Project();
+            $new_project->type_id = Type::inRandomOrder()->first()->id;
             $new_project->name = $faker->name();
             $new_project->slug = Helper::generateSlug($new_project->name, Project::class);
             $new_project->category = $faker->name();
