e09b48e7b4d2774a1b7afa5e3db636b2b02331fa
juri931 <impa.mat@hotmail.it>
2024-05-28 11:34:48 +0200

    "Updated down method in migration to drop foreign key and column 'type_id' from 'projects' table"

--- database/migrations/2024_05_28_093114_update_projects_table.php
+++ database/migrations/2024_05_28_093114_update_projects_table.php
@@ -27,7 +27,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropForeign(['type_id']);
+
+            $table->dropColumn('type_id');
         });
     }
 };
